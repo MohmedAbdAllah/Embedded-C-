2c4a9f1780f1f933250d4213de8298d263159368
Mohamed Ragab <Mohamed.Ragab@Qcentriseg.com>
2023-05-20 19:49:27 +0300

    first try with cmake

--- G8/C++/Session6_/header files/fillArray.cpp
+++ G8/C++/Session6_/header files/fillArray.cpp
@@ -3,12 +3,12 @@
 void fillArray(std::array<float,4>* myArr){
     for(auto i = 0; i < myArr->size(); ++i){
         myArr->at(i) = i;
-        printf("array[%d] = %d",i,i);
+        printf("array[%d] = %d\n",i,i);
     }
 }
 void fillArray(std::array<float,4>& myArr){
     for(auto i = 0; i < myArr.size(); ++i){
         myArr.at(i) = i;
-        printf("array[%d] = %d",i,i);
+        printf("array[%d] = %d\n",i,i);
     }
 }
